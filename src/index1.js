ac091ff7b36227bc500d395626151e5b404ed0d9
Ronald Sandoval <ronsandoval@gmail.com>
2020-05-27 01:36:54 +1000

    Delete unused code

--- src/index1.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-// import ResetPasswordForm from './ResetPasswordForm';
-
-ReactDOM.render(<ResetPasswordForm/>, document.getElementById('root'));
