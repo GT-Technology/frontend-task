1e536e862655439815351b29b9d6590796d2dcad
Ronald Sandoval <ronsandoval@gmail.com>
2020-05-27 01:30:07 +1000

    Task for vinotrust form change password

---
+++ src/index1.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+// import ResetPasswordForm from './ResetPasswordForm';
+
+ReactDOM.render(<ResetPasswordForm/>, document.getElementById('root'));
